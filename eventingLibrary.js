471812d59b333cc4b1ee696cd7aea114d645b199
Danny Rizko <drizko07@gmail.com>
2016-05-20 10:37:37 -0700

    Implements eventingLibrary

---
+++ eventingLibrary.js
@@ -0,0 +1,23 @@
+var mixEvents = function(obj) {
+  var events = {};
+
+  obj.trigger = function (event) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    if(events[event]){
+      for(var i = 0; i < events[event].length; i++){
+        events[event][i].apply(this, args);
+      }
+    }
+  };
+
+  // Register a callback to be fired on this event.
+  obj.on = function (event, callback) {
+    if(!events[event]){
+      events[event] = [];
+    }
+
+    events[event].push(callback);
+  };
+
+  return obj;
+};
